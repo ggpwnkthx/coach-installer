6c4f2469bf0cd6661d6a91251235eb3e7ba77133
ggpwnkthx <ibjessup@gmail.com>
2017-05-03 19:23:15 -0400

    Update and rename ib_pve5.sh to ib_pve.sh

--- ib_pve.sh
+++ ib_pve.sh
@@ -6,7 +6,12 @@ apt-get -y install python-libxml2 make dkms
 wget http://content.mellanox.com/ofed/MLNX_OFED-4.0-2.0.2.0/MLNX_OFED_LINUX-4.0-2.0.2.0-ubuntu17.04-x86_64.iso
 mkdir iso
 mount -o loop MLNX_OFED_LINUX-*.iso iso
-cd iso
-cd DEBS
+cd iso/DEBS
 dpkg -i ofed-scripts* mlnx-ofed-kernel-utils* mlnx-ofed-kernel-dkms* iser-dkms* srp-dkms* libibverbs1* ibverbs-utils* libibverbs-dev* libibverbs1-dbg* libmlx4-1* libmlx4-dev* libmlx4-1-dbg* libmlx5-1* libmlx5-dev* libmlx5-1-dbg* libibumad* libibumad-static* libibumad-devel* ibacm* ibacm-dev* librdmacm1* librdmacm-utils* librdmacm-dev* libibmad*  ibdump* libibmad-static* libibmad-devel* libopensm* opensm* opensm-doc* libopensm-devel* infiniband-diags* infiniband-diags-compat* mft* kernel-mft-dkms* srptools* mlnx-ethtool*
+update-rc.d -f opensmd remove
+sed -i 's/# Default-Start: null/# Default-Start: 2 3 4 5/g' /etc/init.d/opensmd
+update-rc.d opensm defaults
+update-rc.d opensm enable
+update-rc.d -f opensmd remove
 service openibd start
+service opensm start

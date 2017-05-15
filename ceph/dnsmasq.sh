8b4eac7ba71f1cb484626acc9e03d275d46715e8
ggpwnkthx <ibjessup@gmail.com>
2017-05-15 10:42:35 -0400

    Update dnsmasq.sh

--- ceph/dnsmasq.sh
+++ ceph/dnsmasq.sh
@@ -12,3 +12,30 @@ then
 else
   echo "rbd/dhcp id=admin,keyring=/etc/ceph/ceph.client.admin.keyring" | sudo tee /etc/ceph/rbdmap
 fi
+if [ ! -f /etc/systemd/system/rbdmap.service ]
+then
+  sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/ceph/rbdmap.init -O /etc/init.d/rbdmap
+  sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/ceph/rbdmap.service - O /etc/systemd/system/rbdmap.service
+  sudo systemctl enable rbdmap.service
+fi
+systemctl restart rbdmap.service
+sudo apt-get -y install dnsmasq
+if [ ! -d /mnt/ceph/rbd/rbd/dhcp/dnsmasq.d ]
+then
+  sudo cp -r /etc/dnsmasq.d /mnt/ceph/rbd/rbd/dhcp/dnsmasq.d
+  echo "dhcp-range=192.168.0.50,192.168.0.150,255.255.255.0,12h" | sudo tee /mnt/ceph/rbd/rbd/dhcp/dnsmasq.d/range.conf
+  echo "dhcp-leasefile=/mnt/ceph/rbd/rbd/dhcp/dnsmasq.leases" | sudo tee /mnt/ceph/rbd/rbd/dhcp/dnsmasq.d/leases.conf
+fi
+if [ ! -z /mnt/ceph/rbd/rbd/dhcp/dnsmasq.leases ]
+then
+  sudo touch /mnt/ceph/rbd/rbd/dhcp/dnsmasq.leases
+fi
+sudo rm -r /etc/dnsmasq.d
+sudo ln -s /mnt/ceph/rbd/rbd/dhcp/dnsmasq.d /etc
+if [ ! -f /etc/systemd/system/dnsmasq.service ]
+then
+  echo "interface=ib0" | sudo tee --append /etc/dnsmasq.conf
+  sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/ceph/dnsmasq.service -O /etc/systemd/system/dnsmasq.service
+  sudo systemctl enable dnsmasq.service
+fi
+sudo systemctl restart dnsmasq.service

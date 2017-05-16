085fd98191c65a7c21e88f9e553e8485be467681
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 12:46:08 -0400

    Update mon.sh

--- docker/ceph/mon.sh
+++ docker/ceph/mon.sh
@@ -12,15 +12,14 @@ then
        read -p "Username: " username
        sudo scp -r $username@$hostname:/etc/ceph /etc
     esac
-  else
-    echo "Which IP address should be used for the ceph monitor?"
-    ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
-    read -p "IP: " ip
-    sudo docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph/:/var/lib/ceph/ -e MON_IP=$ip -e CEPH_PUBLIC_NETWORK=$(ipcalc $ip -n | grep Network | awk '{print $2}') -name ceph_mon ceph/daemon mon
-    sleep 10
-    sudo chmod 777 /etc/ceph
-    sudo chmod 777 /etc/ceph/*
   fi
+  ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
+  echo "Which IP address should be used for the ceph monitor?
+  "read -p "IP: " ip
+  sudo docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph/:/var/lib/ceph/ -e MON_IP=$ip -e CEPH_PUBLIC_NETWORK=$(ipcalc $ip -n | grep Network | awk '{print $2}') --name ceph_mon ceph/daemon mon
+  sleep 10
+  sudo chmod 777 /etc/ceph
+  sudo chmod 777 /etc/ceph/*
   sudo docker run --restart=always -d ceph_mon
 else
   if [ -z $(sudo docker ps | grep ceph_mon) ]

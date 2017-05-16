2de3335933438021385ed5b523e6a6dd16819827
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 12:53:20 -0400

    Update mon.sh

--- docker/ceph/mon.sh
+++ docker/ceph/mon.sh
@@ -14,8 +14,8 @@ then
     esac
   fi
   ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
-  echo "Which IP address should be used for the ceph monitor?
-  "read -p "IP: " ip
+  echo "Which IP address should be used for the ceph monitor?"
+  read -p "IP: " ip
   sudo docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph/:/var/lib/ceph/ -e MON_IP=$ip -e CEPH_PUBLIC_NETWORK=$(ipcalc $ip -n | grep Network | awk '{print $2}') --name=ceph_mon --restart=always ceph/daemon mon
   sleep 10
   sudo chmod 777 /etc/ceph

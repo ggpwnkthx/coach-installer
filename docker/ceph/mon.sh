7fcbafb7e3a5b5b6b5f8a87b5aebefc7bd57586a
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 10:46:32 -0400

    Update mon.sh

--- docker/ceph/mon.sh
+++ docker/ceph/mon.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#sudo apt-get -y install ipcalc
-sudo docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph:var/lib/ceph -e MON_IP=192.168.0.1 -e CEPH_PUBLIC_NETWORK=192.168.0.0/24 ceph/daemon mon
+sudo docker run -d --net=host \
+-v /etc/ceph:/etc/ceph \
+-v /var/lib/ceph/:/var/lib/ceph/ \
+-e MON_IP=192.168.0.1 \
+-e CEPH_PUBLIC_NETWORK=192.168.0.0/24 \
+ceph/daemon mon

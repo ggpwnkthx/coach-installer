5d0219a667603f361bf85ee5a9877496e48af19f
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 12:55:51 -0400

    Update mon.sh

--- docker/ceph/mon.sh
+++ docker/ceph/mon.sh
@@ -7,10 +7,11 @@ then
     read -n1 -p "Is this the first node? [Y,n]" first
     case $first in
       n|N)
-       echo "Copy ceph configuration from remote host:"
-       read -p "Hostname: " hostname
-       read -p "Username: " username
-       sudo scp -r $username@$hostname:/etc/ceph /etc
+        echo
+        echo "Copy ceph configuration from remote host:"
+        read -p "Hostname: " hostname
+        read -p "Username: " username
+        sudo scp -r $username@$hostname:/etc/ceph /etc
     esac
   fi
   ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'

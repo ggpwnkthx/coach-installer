dfb2197152ce113cc8cff56acaa9054b1146fe9b
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 09:33:43 -0400

    Create infiniband.sh

---
+++ hardware/networking/infiniband.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Add IB modules to /etc/modules and start them
+mods=(mlx4_core mlx4_ib ib_umad ib_uverbs ib_ipoib bonding)
+for i in ${mods[@]}
+do
+  mod=$(cat /etc/modules | grep $i)
+  if [ -z "$mod" ]
+  then
+    echo $i | sudo tee --append /etc/modules
+  fi
+  mod=$(lsmod | grep $i)
+  if [ -z "$mod" ]
+  then
+    sudo modprobe $i
+  fi
+done
+
+# Install IB Subnet Manager
+sudo apt-get update
+sudo apt-get -y install opensm
+sudo update-rc.d -f opensm remove
+sudo update-rc.d opensm defaults
+sudo update-rc.d opensm enable
+sudo service opensm restart

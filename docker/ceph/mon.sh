35fbf119030d5e075fd2688be9fb3df8435adc46
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 13:14:13 -0400

    Update mon.sh

--- docker/ceph/mon.sh
+++ docker/ceph/mon.sh
@@ -14,6 +14,7 @@ then
         sudo scp -r $username@$hostname:/etc/ceph /etc
     esac
   fi
+  echo
   ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
   echo "Which IP address should be used for the ceph monitor?"
   read -p "IP: " ip

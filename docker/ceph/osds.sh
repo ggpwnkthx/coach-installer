cdb93778668335b9c107d98b0d6619cb23a04c35
ggpwnkthx <ibjessup@gmail.com>
2017-05-16 13:09:37 -0400

    Update osds.sh

--- docker/ceph/osds.sh
+++ docker/ceph/osds.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
-lsblk
-read -p "OSD: " device
-read -p "Journal (optional): " journal
-read -p "Zap it OSD target ($device)? [y,N] " zap
-case $zap in
-  y|Y)
-    sudo docker run -d --privileged=true -v /dev/:/dev/ -e OSD_DEVICE=/dev/$device --name=ceph_temp ceph/daemon zap_device
-    while [ ! -z "$(sudo docker ps | grep ceph_temp)" ]
-    do
-      sleep 1
-    done
-    sudo docker rm ceph_temp
-esac
+zap()
+{
+  sudo docker run -d --privileged=true -v /dev/:/dev/ -e OSD_DEVICE=/dev/$1 --name=ceph_temp ceph/daemon zap_device
+  while [ ! -z "$(sudo docker ps | grep ceph_temp)" ]
+  do
+    sleep 1
+  done
+  sudo docker rm ceph_temp
+}
+
+if [ -z $1 ]
+then
+  lsblk
+  read -p "OSD: " device
+  read -p "Zap it OSD target (/dev/$device)? [y,N] " zap
+  case $zap in
+    y|Y)
+      zap $device
+  esac
+  read -p "Journal (optional): " journal
+else
+  device=$1
+  zap $device
+  if [ ! -z $2 ]
+  then
+    if [ "$2" -eq "bluestore" ]
+    then
+      bluestore=y
+    else
+      journal=$2
+    fi
+  fi
+fi
 if [ -z $journal ]
 then
-  read -p "Use Bluestore? [y,N] " bluestore
+  if [ -z $bluestore ]
+  then
+    read -p "Use Bluestore? [y,N] " bluestore
+  fi
   case $bluestore in
     y|Y)
       sudo docker run -d --net=host --privileged=true --pid=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph/:/var/lib/ceph/ -v /dev/:/dev/ -e OSD_DEVICE=/dev/$device -e OSD_TYPE=disk -e OSD_BLUESTORE=1 ceph/daemon osd

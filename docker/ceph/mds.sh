abc1d2115616004577f6903ed233ae67d7b522ff
ggpwnkthx <ibjessup@gmail.com>
2017-05-17 11:33:21 -0400

    Update mds.sh

--- docker/ceph/mds.sh
+++ docker/ceph/mds.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if [ ! -f ceph_preflight.sh ]
+then
+  wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/ceph/preflight.sh -O ceph_preflight.sh
+fi
+chmod +x ceph_preflight.sh
+./ceph_preflight.sh
 sudo docker run -d --name ceph_mds --net=host -v /var/lib/ceph/:/var/lib/ceph/ -v /etc/ceph:/etc/ceph -e CEPHFS_CREATE=1 -e CEPHFS_DATA_POOL_PG=64 -e CEPHFS_METADATA_POOL_PG=64 ceph/daemon mds
+sudo apt-get -y install ceph-common

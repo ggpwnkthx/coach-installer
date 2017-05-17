41498e961381ad79dd5f9f78e374aad79d362c16
ggpwnkthx <ibjessup@gmail.com>
2017-05-17 13:03:53 -0400

    Update mds.sh

--- docker/ceph/mds.sh
+++ docker/ceph/mds.sh
@@ -6,7 +6,6 @@ fi
 chmod +x ceph_preflight.sh
 ./ceph_preflight.sh
 sudo docker run -d --name ceph_mds --net=host -v /var/lib/ceph/:/var/lib/ceph/ -v /etc/ceph:/etc/ceph -e CEPHFS_CREATE=1 -e CEPHFS_DATA_POOL_PG=64 -e CEPHFS_METADATA_POOL_PG=64 ceph/daemon mds
-#!/bin/bash
 if [ ! -f ceph_fs.sh ]
 then
   wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/ceph/fs.sh -O ceph_fs.sh

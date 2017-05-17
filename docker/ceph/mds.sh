0ef0c32e6b1d997f6fb0ad3c40b99b3f7c8f245c
ggpwnkthx <ibjessup@gmail.com>
2017-05-17 09:58:51 -0400

    Create mds.sh

---
+++ docker/ceph/mds.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d --net=host -v /var/lib/ceph/:/var/lib/ceph/ -v /etc/ceph:/etc/ceph -e CEPHFS_CREATE=1 -e CEPHFS_DATA_POOL_PG=64 -e CEPHFS_METADATA_POOL_PG=64 ceph/daemon mds

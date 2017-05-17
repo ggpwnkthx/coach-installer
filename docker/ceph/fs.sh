9ec07dfb1a568640b72fa003e292ca0c2c3e6c7c
ggpwnkthx <ibjessup@gmail.com>
2017-05-17 14:35:06 -0400

    Update fs.sh

--- docker/ceph/fs.sh
+++ docker/ceph/fs.sh
@@ -29,13 +29,27 @@ if [ ! -d "/mnt/ceph/fs" ]
 then
   sudo mkdir /mnt/ceph/fs
 fi
+if [ ! -z "$(systemctl | grep ceph_client.service)" ]
+then
+  sudo systemctl disable ceph_client.service
+fi
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/ceph/client.service -O /etc/systemd/system/ceph_client.servic
+sudo systemctl enable ceph_client.service
 secret=$(sudo ceph-authtool -p /etc/ceph/ceph.client.admin.keyring)
-if [ -z "$(df -h | grep /mnt/ceph/fs)" ]
+if [ ! -z "$(systemctl | grep mnt-cephfs.mount)" ]
 then
-  sudo mount -t ceph $ceph_mons:/ /mnt/ceph/fs -o name=admin,secret=$secret
+  sudo systemctl stop mnt-cephfs.mount
+  sudo systemctl disable mnt-cephfs.mount
 fi
-fstab="$ceph_mons:/ /mnt/ceph/fs ceph name=admin,secret=$secret,noatime,_netdev,x-systemd.automount 0 2"
-if [ -z "$(cat /etc/fstab | grep /mnt/ceph/fs)" ]
-then
-  echo $fstab | sudo tee --append /etc/fstab
-fi
+sudo rm /etc/systemd/system/mnt-cephfs.mount
+echo "[Unit]" | sudo tee /etc/systemd/system/mnt-cephfs.mount
+echo "Description=Mount CephFS" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+echo "After=ceph_client.service" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+echo "[Mount]" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+echo "What=$ceph_mons:/" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+echo "Where=/mnt/ceph/fs" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+echo "Type=ceph" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+echo "Options=name=admin,secret=$secret" | sudo tee --append /etc/systemd/system/mnt-cephfs.mount
+
+sudo systemctl enable mnt-cephfs.mount
+sudo systemctl start mnt-cephfs.mount

d5863146d325de5e6e94d58b314f262a8e5f6242
ggpwnkthx <ibjessup@gmail.com>
2017-05-18 10:56:22 -0400

    Update start.sh

--- docker/ceph/ftp/start.sh
+++ docker/ceph/ftp/start.sh
@@ -15,18 +15,20 @@ fi
 if [ ! -d /mnt/ceph/fs/containers/ftp/config ]
 then
   sudo mkdir -p /mnt/ceph/fs/containers/ftp/config
+  sudo chmod +rw /mnt/ceph/fs/containers/ftp/config
 fi
-if [ ! -f /mnt/ceph/fs/containers/ftp/config/sftp-users.conf ]
+if [ ! -f /mnt/ceph/fs/containers/ftp/config/users.conf ]
 then
-  echo "$user:$password:1001" | sudo tee /mnt/ceph/fs/containers/ftp/config/sftp-users.conf
+  echo "$user:$password:1001" | sudo tee /mnt/ceph/fs/containers/ftp/config/users.conf
   if [ ! -d /mnt/ceph/fs/containers/ftp/data/$user ]
   then
     sudo mkdir -p /mnt/ceph/fs/containers/ftp/data/$user
+    sudo chmod +rw /mnt/ceph/fs/containers/ftp/data/$user
   fi
 else
-  if [ -z "$(cat /mnt/ceph/fs/containers/ftp/config/sftp-users.conf | grep $user)" ]
+  if [ -z "$(cat /mnt/ceph/fs/containers/ftp/config/users.conf | grep $user)" ]
   then
-    uids=($(cat /mnt/ceph/fs/containers/ftp/config/sftp-users.conf | awk '{split($0,a,":"); print a[3]}'))
+    uids=($(cat /mnt/ceph/fs/containers/ftp/config/users.conf | awk '{split($0,a,":"); print a[3]}'))
     if [ -z $uids ]
     then
       uids=1000
@@ -34,15 +36,16 @@ else
     IFS=$'\n'
     hi=$(echo "${uids[*]}" | sort -nr | head -n1)
     uid=$[$hi+1]
-    echo "$user:$password:$uid" | sudo tee --append /mnt/ceph/fs/containers/ftp/config/sftp-users.conf
+    echo "$user:$password:$uid" | sudo tee --append /mnt/ceph/fs/containers/ftp/config/users.conf
     if [ ! -d /mnt/ceph/fs/containers/ftp/data/$user ]
     then
       sudo mkdir -p /mnt/ceph/fs/containers/ftp/data/$user
+      sudo chmod +rw /mnt/ceph/fs/containers/ftp/data/$user
     fi
   else
-    uid=$(cat /mnt/ceph/fs/containers/ftp/config/sftp-users.conf | grep $user | awk '{split($0,a,":"); print a[3]}')
+    uid=$(cat /mnt/ceph/fs/containers/ftp/config/users.conf | grep $user | awk '{split($0,a,":"); print a[3]}')
     sudo sed -i "/^$user/d" /mnt/ceph/fs/containers/sftp/config/users.conf
-    echo "$user:$password:$uid" | sudo tee --append /mnt/ceph/fs/containers/ftp/config/sftp-users.conf
+    echo "$user:$password:$uid" | sudo tee --append /mnt/ceph/fs/containers/ftp/config/users.conf
     if [ ! -d /mnt/ceph/fs/containers/ftp/data/$user ]
     then
       sudo mkdir -p /mnt/ceph/fs/containers/ftp/data/$user
@@ -59,9 +62,9 @@ then
   sudo docker rm ftpd_server
 fi
 sudo docker run -d --name ftpd_server \
-  -v /mnt/ceph/fs/containers/ftp/config/sftp-users.conf:/etc/sftp-users.conf:ro \
+  -v /mnt/ceph/fs/containers/ftp/config/users.conf:/etc/pure-ftpd/passwd/pureftpd.passwd \
   -v /mnt/ceph/fs/containers/ftp/data:/home \
   -p 21:21 \
-  -p 30000-300039:30000-300039 \
+  -p 30000-30039:30000-30039 \
   -e "PUBLICHOST=localhost" \
   stilliard/pure-ftpd:hardened

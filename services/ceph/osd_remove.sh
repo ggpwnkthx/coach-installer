795f27fe2d6338a49d598fe61edc093869bc700c
ggpwnkthx <ibjessup@gmail.com>
2017-05-20 17:24:34 -0400

    Update osd_remove.sh

--- services/ceph/osd_remove.sh
+++ services/ceph/osd_remove.sh
@@ -10,9 +10,11 @@ if [ $hostname == $HOSTNAME ]
 then
   sudo systemctl stop ceph-osd@$1
   sudo umount /var/lib/ceph/osd/ceph-$1
+  sudo rm -r /var/lib/ceph/osd/ceph-$1
 else
   ssh -t $hostname "sudo systemctl stop ceph-osd@$1"
   ssh -t $hostname "sudo umount /var/lib/ceph/osd/ceph-$1"
+  ssh -t $hostname "sudo rm -r /var/lib/ceph/osd/ceph-$1"
 fi
 ceph osd out $1
 ceph osd crush remove osd.$1

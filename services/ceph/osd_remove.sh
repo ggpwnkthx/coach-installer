c66f470189ae2bee5a8002d7a133c1f4f635c5da
ggpwnkthx <ibjessup@gmail.com>
2017-05-20 15:48:53 -0400

    Update osd_remove.sh

--- services/ceph/osd_remove.sh
+++ services/ceph/osd_remove.sh
@@ -5,7 +5,7 @@ then
   "USAGE: ./osd_remove.sh 123"
   exit
 "
-hostname=ceph osd find $1 | awk -F\" '$2 ~ /host/ {print $4}'
+hostname="$(ceph osd find $1 | awk -F\" '$2 ~ /host/ {print $4}')"
 ssh -t $hostname "sudo systemctl stop ceph-osd@$1"
 ssh -t $hostname "sudo umount /var/lib/ceph/osd/ceph-$1"
 ceph osd out $1 

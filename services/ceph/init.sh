4df3003ce73de1bead705d9fe4736686daa1b701
ggpwnkthx <ibjessup@gmail.com>
2017-05-20 11:17:25 -0400

    Update init.sh

--- services/ceph/init.sh
+++ services/ceph/init.sh
@@ -7,8 +7,8 @@ fi
 cd ~/ceph
 ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
 read -p "Which adapter should be used for the ceph cluster? " nic
-addr=$(ifconfig $nic | grep Mask | awk '{print $2}' | awk '{split($0,a,":"); print a[2]}'
-mask=$(ifconfig $nic | grep Mask | awk '{print $4}' | awk '{split($0,a,":"); print a[2]}'
+addr=$(ifconfig $nic | grep Mask | awk '{print $2}' | awk '{split($0,a,":"); print a[2]}')
+mask=$(ifconfig $nic | grep Mask | awk '{print $4}' | awk '{split($0,a,":"); print a[2]}')
 net=$(ipcalc -n $addr $mask | grep Network | awk '{print $2}')
 echo "osd pool default size = 2" >> ceph.conf
 echo "public network = $net" >> ceph.conf

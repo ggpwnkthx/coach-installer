d8ed13730258ff70e3f352dee04aabf5cf558963
ggpwnkthx <ibjessup@gmail.com>
2017-05-20 13:23:00 -0400

    Update init.sh

--- services/ceph/init.sh
+++ services/ceph/init.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+
+echo "$(whoami) ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$(whoami)
+sudo chmod 0440 /etc/sudoers.d/$(whoami)
+
 sudo apt-get -y install net-tools ipcalc ceph-deploy
-if [ ! -d ~/ceph ]
-then
-  mkdir ~/ceph
-fi
-cd ~/ceph
+
 ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
 read -p "Which adapter should be used for the ceph cluster? " nic
 addr=$(ifconfig $nic | grep Mask | awk '{print $2}' | awk '{split($0,a,":"); print a[2]}')
 mask=$(ifconfig $nic | grep Mask | awk '{print $4}' | awk '{split($0,a,":"); print a[2]}')
 net=$(ipcalc -n $addr $mask | grep Network | awk '{print $2}')
+
+wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/hardware/networking/hosts.sh -O hardware_networking_hosts.sh
+chmod +x hardware_networking_hosts.sh
+./hardware_networking_hosts.sh $addr $HOSTNAME
+
+if [ ! -d ~/ceph ]
+then
+  mkdir ~/ceph
+fi
+cd ~/ceph
+ceph-deploy new $HOSTNAME
 echo "osd pool default size = 2" >> ceph.conf
 echo "public network = $net" >> ceph.conf
-sudo sed -i "/$HOSTNAME/ s/.*/$addr\t$HOSTNAME/g" /etc/hosts
-ceph-deploy new $HOSTNAME
 ceph-deploy install $HOSTNAME
 ceph-deploy mon create-initial
 ceph-deploy admin $HOSTNAME
@@ -21,6 +30,3 @@ sudo chmod +r /etc/ceph
 sudo chmod +r /etc/ceph/*
 sudo chmod +r /var/lib/ceph
 sudo chmod +r /var/lib/ceph/*
-
-echo "$(whoami) ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$(whoami)
-sudo chmod 0440 /etc/sudoers.d/$(whoami)

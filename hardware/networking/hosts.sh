11037ad0347c2e8dc3dc5b2d624844b8e1be91f9
ggpwnkthx <ibjessup@gmail.com>
2017-05-20 12:56:42 -0400

    Create hosts.sh

---
+++ hardware/networking/hosts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ -z $1 ]
+then
+  echo "No IP specified."
+  exit
+fi
+if [ -z $2 ]
+then
+  echo "No hostname specified."
+  exit
+fi
+
+if [ -z "$(cat /etc/hosts | grep $1)" ]
+then
+  echo "$1\t$2" | sudo tee --append /etc/hosts
+else
+  sudo sed -i "/^$1/ s/.*/$1\t$2/g" /etc/hosts
+fi
+if [ -z "$(cat /etc/hosts | grep $2)" ]
+then
+  echo "$1\t$2" | sudo tee --append /etc/hosts
+else
+  sudo sed -i "/$2/ s/.*/$1\t$2/g" /etc/hosts
+fi

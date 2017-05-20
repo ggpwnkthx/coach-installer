99a6bc93a832d4a567a6eef89cf23fefa6e5258f
ggpwnkthx <ibjessup@gmail.com>
2017-05-20 15:50:38 -0400

    Update osd_remove.sh

--- services/ceph/osd_remove.sh
+++ services/ceph/osd_remove.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if [ -z $1 }
+if [ -z $1 ]
 then
   echo "No OSD # specified."
-  "USAGE: ./osd_remove.sh 123"
+  echo "USAGE: ./osd_remove.sh 123"
   exit
-"
+fi
 hostname="$(ceph osd find $1 | awk -F\" '$2 ~ /host/ {print $4}')"
 ssh -t $hostname "sudo systemctl stop ceph-osd@$1"
 ssh -t $hostname "sudo umount /var/lib/ceph/osd/ceph-$1"
-ceph osd out $1 
+ceph osd out $1
 ceph osd crush remove osd.$1
 ceph auth del osd.$1
 ceph osd rm $1

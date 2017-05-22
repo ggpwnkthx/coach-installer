2d065b6b20ecd6a1c9dc54d7b66e5dcefecd87ce
ggpwnkthx <ibjessup@gmail.com>
2017-05-22 17:27:26 -0400

    Update preflight.sh

--- services/ceph/preflight.sh
+++ services/ceph/preflight.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
-if [ ! -z $1 ]
+if [ -z $1 ]
 then
+  ceph_mons=($cat /var/lib/dhcp/* | grep "option ceph-mons" | awk '{print $3}')
+  ceph_mons="${ceph_mons::-1}"
+  ceph_mons=echo "$ceph_mons" | tr -d '"'
+  if [ ! -z $ceph_mons ]
+  then
+    IFS=',' read -r -a ceph_mon_ips <<< "$ceph_mons"
+    for ip in ${ceph_mon_ips[@]}
+    do
+      ip=$(echo $ip | awk '{split($0,a":"); print a[1]}')
+      if [ $(ping $ip -c 3 | grep received | awk '{print $4}') -gt 1 ]
+      then
+        hostname=$ip
+      fi
+    done
+  fi
+else
   hostname=$1
 fi
+
+if [ !-z "$(ipcalc $hostname | grep INVALID)" ]
+then
+  if [ -z "$(command -v host)" ]
+  then
+    sudo apt-get -y install host
+    hostname=$(host $hostname | awk '{print $5}' | awk '{split($0,a,'.'); print a[1]}'
+  fi
+fi
+
 if [ ! -f /etc/ceph/ceph.conf ]
 then
   echo "Copy ceph configuration from remote host..."

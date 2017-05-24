02905b5ef84d4f6878e6ffd3a67e94620bd59a7a
ggpwnkthx <ibjessup@gmail.com>
2017-05-24 11:06:58 -0400

    Create deploy.sh

---
+++ docker/provisioner/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/dnsmasq/deploy.sh -O docker_dnsmasq_deploy.sh
+chmod +x docker_dnsmasq_deploy.sh
+./docker_dnsmasq_deploy.sh $@
+
+sudo docker run -d \
+  --name provisioner/lamp --restart=always --net=host \
+  -v /mnt/ceph/fs/containers/provisioner/www:/var/www/html \
+  janes/alpine-lamp

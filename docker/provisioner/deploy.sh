4afa0199aaf203e8767812a52c6b7da55c816fc1
ggpwnkthx <ibjessup@gmail.com>
2017-05-24 14:04:15 -0400

    Update deploy.sh

--- docker/provisioner/deploy.sh
+++ docker/provisioner/deploy.sh
@@ -12,6 +12,20 @@ then
   sudo docker rm provisioner_lamp
 fi
 
+if [ ! -d /mnt/ceph/fs/containers/provisioner/www ]
+then
+  sudo mkdir /mnt/ceph/fs/containers/provisioner/www
+fi
+
+sudo rm /mnt/ceph/fs/containers/provisioner/www/boot/boot2docker
+sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/boot/boot2docker
+sudo wget $(wget https://api.github.com/repos/boot2docker/boot2docker/releases -O - | grep browser_download_url | head -n 1 | awk '{print $2}' | tr -d '"' ) -O /mnt/ceph/fs/containers/provisioner/www/boot/boot2docker/boot2docker.iso
+sudo mkdir -p /mnt/boot2docker
+sudo mount -o loop /mnt/ceph/fs/containers/provisioner/www/boot/boot2docker/boot2docker.iso /mnt/boot2docker
+sudo cp /mnt/boot2docker/boot/initrd.img /mnt/ceph/fs/containers/provisioner/www/boot/boot2docker/initrd.img
+sudo cp /mnt/boot2docker/boot/vmlinuz64 /mnt/ceph/fs/containers/provisioner/www/boot/boot2docker/vmlinuz64
+
+sudo chmod -R +rw /mnt/ceph/fs/containers/provisioner
 sudo docker run -d \
   --name provisioner_lamp --restart=always --net=host \
   -v /mnt/ceph/fs/containers/provisioner/www:/www \

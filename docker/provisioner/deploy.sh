16157b2395fde5283e6d0b3ddcb4a581c69b1989
ggpwnkthx <ibjessup@gmail.com>
2017-05-24 11:15:11 -0400

    Update deploy.sh

--- docker/provisioner/deploy.sh
+++ docker/provisioner/deploy.sh
@@ -3,6 +3,15 @@ wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/dnsmasq/dep
 chmod +x docker_dnsmasq_deploy.sh
 ./docker_dnsmasq_deploy.sh $@
 
+if [ ! -z "$(sudo docker ps | grep provisioner_lamp)" ]
+then
+  sudo docker kill provisioner_lamp
+fi
+if [ ! -z "$(sudo docker ps -a | grep provisioner_lamp)" ]
+then
+  sudo docker rm provisioner_lamp
+fi
+
 sudo docker run -d \
   --name provisioner_lamp --restart=always --net=host \
   -v /mnt/ceph/fs/containers/provisioner/www:/var/www/html \

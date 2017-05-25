cab607e85952fc7e8485e42b9e77469860e37e25
ggpwnkthx <ibjessup@gmail.com>
2017-05-25 10:40:55 -0400

    Update deploy.sh

--- docker/provisioner/deploy.sh
+++ docker/provisioner/deploy.sh
@@ -17,12 +17,24 @@ then
   sudo mkdir /mnt/ceph/fs/containers/provisioner/www
 fi
 
+#sudo apt-get -y install build-essentials git curl
+#git clone https://github.com/rancher/os.git
+#cd os-kernel
+#sudo make
+#cd ..
+
 sudo rm /mnt/ceph/fs/containers/provisioner/www/boot/rancher
 sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/boot/rancher
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/ipxe.php -O /mnt/ceph/fs/containers/provisioner/www/index.php
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/cloud-init -O /mnt/ceph/fs/containers/provisioner/www/boot/rancher/cloud-init
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/ipxe.php -O /mnt/ceph/fs/containers/provisioner/www/index.php
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/cloud-init -O /mnt/ceph/fs/containers/provisioner/www/boot/rancher/cloud-init
 sudo wget https://releases.rancher.com/os/latest/vmlinuz -O /mnt/ceph/fs/containers/provisioner/www/boot/rancher/vmlinuz
-sudo wget https://releases.rancher.com/os/latest/initrd -O /mnt/ceph/fs/containers/provisioner/www/boot/rancher/initrd
+wget https://releases.rancher.com/os/latest/initrd -O initrd.lzma
+sudo apt-get -y install lzma
+unlzma initrd.lzma
+mkdir rancheros_initrd
+cd rancheros_initrd
+cpio -id < ../initrd
+
 
 sudo chmod -R +r /mnt/ceph/fs/containers/provisioner
 sudo docker run -d \

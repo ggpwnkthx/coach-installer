39a6b9889ad85f8ec75169a9a2c01407eb4b79ea
ggpwnkthx <ibjessup@gmail.com>
2017-05-29 13:10:49 -0400

    Update init.sh

--- services/ceph/init.sh
+++ services/ceph/init.sh
@@ -2,23 +2,22 @@
 echo "$(whoami) ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$(whoami)
 sudo chmod 0440 /etc/sudoers.d/$(whoami)
 
-clear
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
-echo "COACH - Cluster Of Arbitrary, Cheap, Hardware"
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
-echo "Ceph Initialization - $HOSTNAME"
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-
 ./download_and_run "services/ceph/purge.sh"
 
 sudo apt-get -y install net-tools ipcalc ceph-deploy
-if [ -f /etc/networking/interfaces.d/storage ]
+if [ -f /etc/network/interfaces.d/storage ]
 then
-  addr=$(cat /etc/networking/interfaces.d/storage | grep address | awk '{print $2}')
-  mask=$(cat /etc/networking/interfaces.d/storage | grep netmaks | awk '{print $2}')
+  addr=$(cat /etc/network/interfaces.d/storage | grep address | awk '{print $2}')
+  mask=$(cat /etc/network/interfaces.d/storage | grep netmaks | awk '{print $2}')
 else
   if [ -z $1 ]
   then
+    clear
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+    echo "COACH - Cluster Of Arbitrary, Cheap, Hardware"
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+    echo "Ceph Initialization - $HOSTNAME"
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
     read -p "Which adapter should be used for the ceph cluster? " nic
   else
@@ -42,9 +41,7 @@ if [ ! -z $2 ]
 then
   echo "osd_journal_size = $2" >> ceph.conf
 fi
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ceph/admin.sh -O services_ceph_admin.sh
-chmod +x services_ceph_admin.sh
-./services_ceph_admin.sh
+./download_and_run services/ceph/admin.sh"
 
 ceph-deploy mon create-initial
 sudo chmod +rw /etc/ceph

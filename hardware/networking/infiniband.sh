993a79593c039406c3bbca4ab7b3118cc7338f06
ggpwnkthx <ibjessup@gmail.com>
2017-05-29 11:04:16 -0400

    Update infiniband.sh

--- hardware/networking/infiniband.sh
+++ hardware/networking/infiniband.sh
@@ -16,9 +16,12 @@ do
 done
 
 # Install IB Subnet Manager
-sudo apt-get update
-sudo apt-get -y install ifenslave-2.6 opensm
-sudo update-rc.d -f opensm remove
-sudo update-rc.d opensm defaults
-sudo update-rc.d opensm enable
-sudo service opensm restart
+if -z "$(command -v opensm)"]
+then
+  sudo apt-get update
+  sudo apt-get -y install ifenslave-2.6 opensm
+  sudo update-rc.d -f opensm remove
+  sudo update-rc.d opensm defaults
+  sudo update-rc.d opensm enable
+  sudo service opensm restart
+fi

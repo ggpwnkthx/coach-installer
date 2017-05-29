b3747c308465a739bb2409c87ce1013640ad6d6c
ggpwnkthx <ibjessup@gmail.com>
2017-05-29 13:07:38 -0400

    Update init.sh

--- services/ceph/init.sh
+++ services/ceph/init.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
-
 echo "$(whoami) ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$(whoami)
 sudo chmod 0440 /etc/sudoers.d/$(whoami)
 
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ceph/purge.sh -O services_ceph_purge.sh
-chmod +x services_ceph_purge.sh
-./services_ceph_purge.sh
+clear
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+echo "COACH - Cluster Of Arbitrary, Cheap, Hardware"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+echo "Ceph Initialization - $HOSTNAME"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
+./download_and_run "services/ceph/purge.sh"
 
 sudo apt-get -y install net-tools ipcalc ceph-deploy
-if [ -z $1 ]
+if [ -f /etc/networking/interfaces.d/storage ]
 then
-  ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
-  read -p "Which adapter should be used for the ceph cluster? " nic
+  addr=$(cat /etc/networking/interfaces.d/storage | grep address | awk '{print $2}')
+  mask=$(cat /etc/networking/interfaces.d/storage | grep netmaks | awk '{print $2}')
 else
-  nic=$1
+  if [ -z $1 ]
+  then
+    ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
+    read -p "Which adapter should be used for the ceph cluster? " nic
+  else
+    nic=$1
+  fi
+  addr=$(ifconfig $nic | grep Mask | awk '{print $2}' | awk '{split($0,a,":"); print a[2]}')
+  mask=$(ifconfig $nic | grep Mask | awk '{print $4}' | awk '{split($0,a,":"); print a[2]}')
+  net=$(ipcalc -n $addr $mask | grep Network | awk '{print $2}')
 fi
-addr=$(ifconfig $nic | grep Mask | awk '{print $2}' | awk '{split($0,a,":"); print a[2]}')
-mask=$(ifconfig $nic | grep Mask | awk '{print $4}' | awk '{split($0,a,":"); print a[2]}')
-net=$(ipcalc -n $addr $mask | grep Network | awk '{print $2}')
-
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/hardware/networking/hosts.sh -O hardware_networking_hosts.sh
-chmod +x hardware_networking_hosts.sh
-./hardware_networking_hosts.sh $addr $HOSTNAME
+./download_and_run hardware/networking/hosts.sh" $addr $HOSTNAME
 
 if [ ! -d ~/ceph ]
 then

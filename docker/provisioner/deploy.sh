2aab62b3ae17206c982caa0528238c3978e7b596
ggpwnkthx <ibjessup@gmail.com>
2017-05-30 09:43:41 -0400

    Update deploy.sh

--- docker/provisioner/deploy.sh
+++ docker/provisioner/deploy.sh
@@ -3,28 +3,32 @@ wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner
 chmod +x docker_dnsmasq_deploy.sh
 ./docker_dnsmasq_deploy.sh $@
 
-if [ ! -z "$(sudo docker ps | grep provisioner_lamp)" ]
-then
-  sudo docker kill provisioner_lamp
-fi
-if [ ! -z "$(sudo docker ps -a | grep provisioner_lamp)" ]
-then
-  sudo docker rm provisioner_lamp
-fi
+services=(provisioner_lamp provisioner_tftp)
+for s in $services
+do
+  if [ ! -z "$(sudo docker ps -a | grep $s)" ]
+  then
+    sudo docker rm -f $s
+  fi
+done
 
 if [ ! -d /mnt/ceph/fs/containers/provisioner/www ]
 then
   sudo mkdir /mnt/ceph/fs/containers/provisioner/www
 fi
-
-sudo rm /mnt/ceph/fs/containers/provisioner/www/boot/coreos
-sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/boot/coreos
-sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/ipxe.php -O /mnt/ceph/fs/containers/provisioner/www/index.php
-sudo wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O /mnt/ceph/fs/containers/provisioner/www/boot/coreos/vmlinuz
-sudo wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O /mnt/ceph/fs/containers/provisioner/www/boot/coreos/image.cpio.gz
-
+if [ ! -d /mnt/ceph/fs/containers/provisioner/tftp ]
+then
+  sudo mkdir /mnt/ceph/fs/containers/provisioner/tftp
+fi
+wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/ipxe.php -O /mnt/ceph/fs/containers/provisioner/www/index.php
 sudo chmod -R +r /mnt/ceph/fs/containers/provisioner
+
 sudo docker run -d \
   --name provisioner_lamp --restart=always --net=host \
   -v /mnt/ceph/fs/containers/provisioner/www:/www \
   janes/alpine-lamp
+  
+sudo docker run -d \
+  --name provisioner_tftp --restart=always --net=host \ 
+  -v /mnt/ceph/fs/containers/provisioner/tftp: \
+  pghalliday/tftp

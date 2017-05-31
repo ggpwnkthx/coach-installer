506da7c6d1d4571e4228e9827a90dd679d3cebd0
ggpwnkthx <ibjessup@gmail.com>
2017-05-31 10:21:15 -0400

    Update deploy.sh

--- docker/provisioner/deploy.sh
+++ docker/provisioner/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./download_and_run "docker/provisioner/dnsmasq/deploy.sh" $@
 
-services=(provisioner_lamp provisioner_tftp)
+services=(provisioner_lamp)
 for s in ${services[@]}
 do
   if [ ! -z "$(sudo docker ps -a | grep $s)" ]
@@ -14,13 +14,10 @@ if [ ! -d /mnt/ceph/fs/containers/provisioner/www ]
 then
   sudo mkdir /mnt/ceph/fs/containers/provisioner/www
 fi
-if [ ! -d /mnt/ceph/fs/containers/provisioner/tftp ]
-then
-  sudo mkdir /mnt/ceph/fs/containers/provisioner/tftp
-fi
 sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/ipxe.php -O /mnt/ceph/fs/containers/provisioner/www/index.php
 
-cp /etc/initramfs-tools initramfs-tools
+cp -r /etc/initramfs-tools initramfs-tools
+sed -i '/^MODULES=/s/=.*/netboot/' initramfs-tools/initramfs.conf
 echo "mlx4_core" | tee --append initramfs-tools/modules
 echo "mlx4_ib" | tee --append initramfs-tools/modules
 echo "ib_umad" | tee --append initramfs-tools/modules
@@ -30,14 +27,15 @@ mkinitramfs -d initramfs-tools -o initrd
 sudo mv initrd /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu/initrd
 sudo cp /boot/vmlinuz-`uname -r` /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu/vmlinuz
 
+wget https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64.squashfs -O squashfs
+unsquashfs squashfs
+
+sudo wget https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-initrd-generic -O /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu/initrd
+sudo wget https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-vmlinuz-generic -O /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu/vmlinuz
+
 sudo chmod -R +r /mnt/ceph/fs/containers/provisioner
 
 sudo docker run -d \
   --name provisioner_lamp --restart=always --net=host \
   -v /mnt/ceph/fs/containers/provisioner/www:/www \
   janes/alpine-lamp
-
-sudo docker run -d \
-  --name provisioner_tftp --restart=always --net=host \
-  -v /mnt/ceph/fs/containers/provisioner/tftp:/var/tftpboot \
-  pghalliday/tftp

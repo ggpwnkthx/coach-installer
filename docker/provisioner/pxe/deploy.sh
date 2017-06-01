137decb4a22712ecf3f5cea5d6137fd305797f41
ggpwnkthx <ibjessup@gmail.com>
2017-06-01 11:31:36 -0400

    Update deploy.sh

--- docker/provisioner/pxe/deploy.sh
+++ docker/provisioner/pxe/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/index.php -O /mnt/ceph/fs/containers/provisioner/www/index.php
-
 ./download_and_run "docker/provisioner/pxe/vmlinuz.sh"
 ./download_and_run "docker/provisioner/pxe/initrd.sh"
 ./download_and_run "docker/provisioner/pxe/filesystem.sh"

048c2e663684935045b7aa128be840d5f3450413
ggpwnkthx <ibjessup@gmail.com>
2017-06-01 08:30:21 -0400

    Update deploy.sh

--- docker/provisioner/deploy.sh
+++ docker/provisioner/deploy.sh
@@ -27,6 +27,8 @@ echo "mlx4_ib" | tee --append initramfs-tools/modules
 echo "ib_umad" | tee --append initramfs-tools/modules
 echo "ib_uverbs" | tee --append initramfs-tools/modules
 echo "ib_ipoib" | tee --append initramfs-tools/modules
+wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/initramfs.script -O initramfs-tools/scripts/init-bottom/network
+chmod +x initramfs-tools/scripts/init-bottom/network
 mkinitramfs -d initramfs-tools -o initrd
 sudo mv initrd /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu/initrd
 

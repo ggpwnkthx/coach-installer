181a5176a27632547b740c0ecb432ffdef4502e4
ggpwnkthx <ibjessup@gmail.com>
2017-06-01 10:53:37 -0400

    Create deploy.sh

---
+++ docker/provisioner/lamp/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+services=(provisioner_lamp)
+for s in ${services[@]}
+do
+  if [ ! -z "$(sudo docker ps -a | grep $s)" ]
+  then
+    sudo docker rm -f $s
+  fi
+done
+if [ ! -d /mnt/ceph/fs/containers/provisioner/www ]
+then
+  sudo mkdir /mnt/ceph/fs/containers/provisioner/www
+fi
+sudo docker run -d \
+  --name provisioner_lamp --restart=always --net=host \
+  -v /mnt/ceph/fs/containers/provisioner/www:/www \
+  janes/alpine-lamp

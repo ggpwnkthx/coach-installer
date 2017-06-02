4ba8e61ed3ebfe6095f30e0811b3973a39689e28
ggpwnkthx <ibjessup@gmail.com>
2017-06-02 13:22:06 -0400

    Update initrd.sh

--- docker/provisioner/pxe/initrd.sh
+++ docker/provisioner/pxe/initrd.sh
@@ -5,11 +5,13 @@ echo "COACH - Cluster Of Arbitrary, Cheap, Hardware"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 echo "Creating a better initrd file"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
 if [ -d initramfs-tools ]
 then
   sudo rm -r initramfs-tools
 fi
 cp -r /etc/initramfs-tools initramfs-tools
+
 sed -i '/^MODULES=/s/=.*/=netboot/' initramfs-tools/initramfs.conf
 echo "mlx4_core" | tee --append initramfs-tools/modules
 echo "mlx4_ib" | tee --append initramfs-tools/modules
@@ -19,4 +21,20 @@ echo "ib_ipoib" | tee --append initramfs-tools/modules
 wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/initramfs.script -O initramfs-tools/scripts/init-bottom/network
 chmod +x initramfs-tools/scripts/init-bottom/network
 mkinitramfs -d initramfs-tools -o initrd
+
+if [ -d initrd-root ]
+then
+  sudo rm -r initrd-root
+fi
+mkdir initrd-root
+cd initrd-root
+zcat ../initrd | cpio -id
+cp /usr/bin/unsquashfs bin/
+cp /lib/x86_64-linux-gnu/libm.so.6 lib/x86_64-linux-gnu/
+cp /lib/x86_64-linux-gnu/liblzma.so.5 lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/liblz4.so.1 lib/x86_64-linux-gnu/
+
+find . | cpio --create --format='newc' > ../initrd
+cd ..
+
 sudo mv initrd /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu/initrd

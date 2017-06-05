3816f20a5625c159376e37d9cca08730b877644e
ggpwnkthx <ibjessup@gmail.com>
2017-06-05 16:58:25 -0400

    Update fs.sh

--- services/ceph/fs.sh
+++ services/ceph/fs.sh
@@ -5,7 +5,7 @@ then
 fi
 if [ ! -f /etc/ceph/ceph.conf ]
 then
-  ../../download_and_run "services/ceph/preflight.sh"
+  ./download_and_run "services/ceph/preflight.sh"
 fi
 
 ceph_mon_ls=($(sudo ceph mon dump | grep mon | awk '{print $2}' | awk '{split($0,a,"/"); print a[1]}'))

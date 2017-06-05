ead8000ff90433e243b8779c73087771b21d70f5
ggpwnkthx <ibjessup@gmail.com>
2017-06-05 15:25:11 -0400

    Update ceph.sh

--- bootstrap/ceph.sh
+++ bootstrap/ceph.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./download_and_run "services/ceph/mon.sh"
+./download_and_run "services/ceph/disks.sh"
+./download_and_run "services/ceph/mds.sh"
+./download_and_run "services/ceph/fs.sh"

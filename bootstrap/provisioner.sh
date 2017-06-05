a8b6cc6999a94b3894f159ce14ed01c647622773
ggpwnkthx <ibjessup@gmail.com>
2017-06-05 17:27:12 -0400

    Update provisioner.sh

--- bootstrap/provisioner.sh
+++ bootstrap/provisioner.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-./download_and_run "docker/deploy.sh"
+
+if [ -z "$(command -v docker)" ]
+then
+  ./download_and_run "docker/deploy.sh"
+fi
+
 ./download_and_run "docker/provisioner/deploy.sh"

bc1e2fa198bb4cdf2867b142eec2920612a5da0c
ggpwnkthx <ibjessup@gmail.com>
2017-06-06 16:35:02 -0400

    Update initrd.sh

--- docker/provisioner/pxe/initrd.sh
+++ docker/provisioner/pxe/initrd.sh
@@ -6,11 +6,37 @@ printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 echo "Creating a better initrd file"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
+if [ -d initramfs-tools ]
+then
+  sudo rm -r initramfs-tools
+fi
+cp -r /etc/initramfs-tools initramfs-tools
+
+sed -i '/^MODULES=/s/=.*/=netboot/' initramfs-tools/initramfs.conf
+echo "mlx4_core" | tee --append initramfs-tools/modules
+echo "mlx4_ib" | tee --append initramfs-tools/modules
+echo "ib_umad" | tee --append initramfs-tools/modules
+echo "ib_uverbs" | tee --append initramfs-tools/modules
+echo "ib_ipoib" | tee --append initramfs-tools/modules
+mkinitramfs -d initramfs-tools -o initrd
+
+if [ -d initrd-mod ]
+then
+  sudo rm -r initrd-mod
+fi
+mkdir initrd-mod
+
+cd initrd-mod
+
+
 if [ -d initrd-root ]
 then
   sudo rm -r initrd-root
 fi
 mkdir initrd-root
+zcat ../initrd | cpio -id
+cd ..
+rm initrd
 
 wget https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-initrd-generic -O initrd.lzma
 unlzma initrd.lzma
@@ -23,8 +49,7 @@ echo "mlx4_ib" | tee --append conf/modules
 echo "ib_umad" | tee --append conf/modules
 echo "ib_uverbs" | tee --append conf/modules
 echo "ib_ipoib" | tee --append conf/modules
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/initramfs.script -O scripts/init-bottom/network
-chmod +x scripts/init-bottom/network
+cp -r ../initrd-mod/lib/modules lib/
 
 find . | cpio --create --format='newc' > ../initrd
 cd ..

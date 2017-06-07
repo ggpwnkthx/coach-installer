d88a11e4a08905076e045ee48708c068eb30e368
ggpwnkthx <ibjessup@gmail.com>
2017-06-07 13:16:00 -0400

    Update initrd.sh

--- docker/provisioner/pxe/initrd.sh
+++ docker/provisioner/pxe/initrd.sh
@@ -6,6 +6,34 @@ printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 echo "Creating a better initrd file"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
+if [ -d initrd-root ]
+then
+  sudo rm -r initrd-root
+fi
+wget https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-initrd-generic -O initrd.lzma
+unlzma initrd.lzma
+mkdir initrd-root
+cd initrd-root
+cat ../initrd | cpio -id
+wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/initramfs.script -O scripts/init-bottom/network
+chmod +x scripts/init-bottom/network
+echo '/scripts/init-bottom/network "$@"' | tee --append scripts/init-bottom/ORDER
+echo '[ -e /conf/param.conf ] && . /conf/param.conf' | tee --append scripts/init-bottom/ORDER
+sed -i '/^MODULES=/s/=.*/=netboot/' conf/initramfs.conf
+echo "mlx4_core" | tee --append conf/modules
+echo "mlx4_ib" | tee --append conf/modules
+echo "ib_umad" | tee --append conf/modules
+echo "ib_uverbs" | tee --append conf/modules
+echo "ib_ipoib" | tee --append conf/modules
+ver=$(ls lib/modules/)
+if [ ! -d /lib/modules/$ver ]
+then
+  sudo apt-get -y install linux-headers-$ver
+fi
+
+cd ..
+rm initrd
+
 if [ -d initramfs-tools ]
 then
   sudo rm -r initramfs-tools
@@ -18,7 +46,7 @@ echo "mlx4_ib" | tee --append initramfs-tools/modules
 echo "ib_umad" | tee --append initramfs-tools/modules
 echo "ib_uverbs" | tee --append initramfs-tools/modules
 echo "ib_ipoib" | tee --append initramfs-tools/modules
-mkinitramfs -d initramfs-tools -o initrd
+mkinitramfs -d initramfs-tools -o initrd $ver
 
 if [ -d initrd-mod ]
 then
@@ -31,37 +59,10 @@ zcat ../initrd | cpio -id
 cd ..
 rm initrd
 
-if [ -d initrd-root ]
-then
-  sudo rm -r initrd-root
-fi
+cp -r initrd-mod/lib/modules/*/kernel/drivers initrd-root/lib/modules/*/kernel/
+diff initrd-root/lib/modules/*/modules.dep initrd-mod/lib/modules/*/modules.dep | grep "> " | sed 's/> //g' | tee --append initrd-root/lib/modules/*/modules.dep
 
-wget https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-initrd-generic -O initrd.lzma
-unlzma initrd.lzma
-mkdir initrd-root
-cd initrd-root
-cat ../initrd | cpio -id
-
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/initramfs.script -O scripts/init-bottom/network
-chmod +x scripts/init-bottom/network
-echo '/scripts/init-bottom/network "$@"' | tee --append scripts/init-bottom/ORDER
-echo '[ -e /conf/param.conf ] && . /conf/param.conf' | tee --append scripts/init-bottom/ORDER
-
-sed -i '/^MODULES=/s/=.*/=netboot/' conf/initramfs.conf
-echo "mlx4_core" | tee --append conf/modules
-echo "mlx4_ib" | tee --append conf/modules
-echo "ib_umad" | tee --append conf/modules
-echo "ib_uverbs" | tee --append conf/modules
-echo "ib_ipoib" | tee --append conf/modules
-
-cp -r ../initrd-mod/lib/modules/*/kernel/drivers lib/modules/*/kernel/
-diff lib/modules/*/modules.dep ../initrd-mod/lib/modules/*/modules.dep | grep "> " | sed 's/> //g' | tee --append lib/modules/*/modules.dep
 cd ..
-ver=$(ls initrd-root/lib/modules/)
-if [ ! -d /lib/modules/$ver ]
-then
-  sudo apt-get -y install linux-headers-$ver
-fi
 depmod $ver -b initrd-root -E /lib/modules/$ver/build/Module.symvers
 cd initrd-root
 

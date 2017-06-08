5ffc753f967066e1eed42a92ec2d0df228e01be9
ggpwnkthx <ibjessup@gmail.com>
2017-06-08 14:12:45 -0400

    Create fs.sh

--- services/ceph/fs.sh
+++ services/ceph/fs.sh
@@ -12,9 +12,11 @@ if [ ! -z "$(systemctl | grep ceph-client.service)" ]
 then
   sudo systemctl disable ceph-client.service
 fi
-sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ceph/client.service -O /etc/systemd/system/ceph-client.service
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ceph/ceph-client.service -O /etc/systemd/system/ceph-client.service
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ceph/mnt-ceph-fs.sh -O /etc/ceph/mnt-ceph-fs.sh
 sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ceph/mnt-ceph-fs.service -O /etc/systemd/system/mnt-ceph-fs.service
 sudo systemctl daemon-reload
 sudo systemctl enable ceph-client.service
+sudo systemctl start ceph-client.service
 sudo systemctl enable mnt-ceph-fs.service
 sudo systemctl start mnt-ceph-fs.service

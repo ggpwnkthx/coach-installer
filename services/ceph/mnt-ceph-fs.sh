0474a8c483015cd2f848c5a81e36255abd231def
ggpwnkthx <ibjessup@gmail.com>
2017-06-08 14:38:40 -0400

    Create mnt-ceph-fs.sh

--- services/ceph/mnt-ceph-fs.sh
+++ services/ceph/mnt-ceph-fs.sh
@@ -1,29 +1,36 @@
+
 #!/bin/bash
 mnt_path=/mnt/ceph/fs
 
 case $1 in
   start)
-    name=admin
-    ceph_mon_ls=($(sudo ceph mon dump | grep mon | awk '{print $2}' | awk '{split($0,a,"/"); print a[1]}'))
-    ceph_mons=""
-    for i in ${ceph_mon_ls[@]}
-    do
-      if [ -z $ceph_mons ]
-      then
-        ceph_mons="$i"
-      else
-        ceph_mons="$ceph_mons,$i"
-      fi
-    done
-    if [ ! -d $mnt_path ]
+    if [ -z "$(df -h | grep $mnt_path)" ]
     then
-      mkdir -p $mnt_path
+      name=admin
+      ceph_mon_ls=($(sudo ceph mon dump | grep mon | awk '{print $2}' | awk '{split($0,a,"/"); print a[1]}'))
+      ceph_mons=""
+      for i in ${ceph_mon_ls[@]}
+      do
+        if [ -z $ceph_mons ]
+        then
+          ceph_mons="$i"
+        else
+          ceph_mons="$ceph_mons,$i"
+        fi
+      done
+      if [ ! -d $mnt_path ]
+      then
+        mkdir -p $mnt_path
+      else
+        rm -r $mnt_path/*
+      fi
+      secret=$(ceph-authtool -p /etc/ceph/ceph.client.$name.keyring)
+      while [ -z "$(df -h | grep $mnt_path)" ]
+      do
+        echo "mount -t ceph $ceph_mons:/ $mnt_path -o name=$name,secret=$secret"
+        mount -t ceph $ceph_mons:/ $mnt_path -o name=$name,secret=$secret
+      done
     fi
-    secret=$(ceph-authtool -p /etc/ceph/ceph.client.admin.keyring)
-    while [ -z "$(df -h | grep $mnt_path)" ]
-    do
-      mount -t ceph $ceph_mons:/ $mnt_path -o name=$name,secret=$secret
-    done
     ;;
   stop)
     umount $mnt_path

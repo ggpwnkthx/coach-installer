1eb19c3df31f7d85e6b31678f98f8238ac97496b
ggpwnkthx <ibjessup@gmail.com>
2017-06-08 15:24:28 -0400

    Create provisioner.sh

--- bootstrap/provisioner.sh
+++ bootstrap/provisioner.sh
@@ -3,7 +3,8 @@
 if [ -z "$(command -v docker)" ]
 then
   ./download_and_run "docker/deploy.sh"
-  sudo sed -i '/^after:/ s/$/ mnt-ceph-fs.mount/' /etc/systemd/system/multi-user.target.wants/docker.service
+  sudo sed -i '/^after:/ s/$/ mnt-ceph-fs.service/' /etc/systemd/system/multi-user.target.wants/docker.service
+  sudo systemctl daemon-reload
   sudo systemctl reenable docker.service
 fi
 

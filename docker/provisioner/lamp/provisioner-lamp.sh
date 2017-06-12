c469a21aab189fcc4c7c09779d5f2f2158ca0be6
ggpwnkthx <ibjessup@gmail.com>
2017-06-12 12:15:04 -0400

    Create provisioner-lamp.sh

---
+++ docker/provisioner/lamp/provisioner-lamp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+case $1 in
+  start)
+    if [ ! -z "$(docker ps -a | grep provisioner_lamp)" ]
+    then
+      docker rm -f provisioner_lamp
+    fi
+    docker run -d \
+      --name provisioner_lamp --net=host \
+      -v /mnt/ceph/fs/containers/provisioner/www:/www \
+      janes/alpine-lamp
+    ;;
+  stop)
+    docker stop provisioner_lamp
+    docker rm -f provisioner_lamp
+    ;;
+  status)
+    docker ps -a | grep provisioner_lamp
+    ;;
+esac

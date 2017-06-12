f0ba40d511752070646dca87553c3210588c9c94
ggpwnkthx <ibjessup@gmail.com>
2017-06-12 12:24:12 -0400

    Create deploy.sh

--- docker/provisioner/lamp/deploy.sh
+++ docker/provisioner/lamp/deploy.sh
@@ -12,19 +12,9 @@ then
   sudo mkdir /mnt/ceph/fs/containers/provisioner/www
 fi
 
-echo "#!/bin/bash" | sudo tee /etc/ceph/provisioner-lamp.sh
-echo "case \$1 in" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "  start)" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    docker run -d \\" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "      --name provisioner_lamp --net=host \\" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "      -v /mnt/ceph/fs/containers/provisioner/www:/www \\" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "      janes/alpine-lamp" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    ;;" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "  stop)" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    docker stop provisioner_lamp" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    docker rm -f provisioner_lamp" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    ;;" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "  status)" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    docker ps -a | grep provisioner_lamp" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "    ;;" | sudo tee --append /etc/ceph/provisioner-lamp.sh
-echo "esac" | sudo tee --append /etc/ceph/provisioner-lamp.sh
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/lamp/provisioner-lamp.sh -O /etc/ceph/provisioner-lamp.sh
+sudo chmod +x /etc/ceph/provisioner-lamp.sh
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/lamp/provisioner-lamp.service -O /etc/systemd/system/provisioner-lamp.service
+sudo systemctl daemon-reload
+sudo systemctl enable provisioner-lamp.service
+sudo systemctl start provisioner-lamp.service

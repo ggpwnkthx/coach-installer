3166f500630993317f1291658dd69176e829e79f
ggpwnkthx <ibjessup@gmail.com>
2017-06-15 09:58:56 -0400

    Create deploy.sh

--- docker/provisioner/pxe/deploy.sh
+++ docker/provisioner/pxe/deploy.sh
@@ -4,23 +4,18 @@ then
   sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/boot/ubuntu
 fi
 sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/ipxe.php -O /mnt/ceph/fs/containers/provisioner/www/ipxe.php
+if [ ! -d /mnt/ceph/fs/containers/provisioner/www/meta-data ]
+then
+  sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/meta-data
+fi
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/meta-data.php -O /mnt/ceph/fs/containers/provisioner/www/meta-data/index.php
 
-if [ ! -d /mnt/ceph/fs/containers/provisioner/www/2009-04-04/meta-data ]
+if [ ! -d /mnt/ceph/fs/containers/provisioner/www/user-data ]
 then
-  sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/2009-04-04/meta-data
+  sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/user-data
 fi
-if [ ! -d /mnt/ceph/fs/containers/provisioner/www/2009-04-04/meta-data/instance-id ]
-then
-  sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/2009-04-04/meta-data/instance-id
-fi
-if [ ! -d /mnt/ceph/fs/containers/provisioner/www/2009-04-04/meta-data/hostname ]
-then
-  sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/2009-04-04/meta-data/hostname
-fi
-if [ ! -d /mnt/ceph/fs/containers/provisioner/www/2009-04-04/user-data ]
-then
-  sudo mkdir -p /mnt/ceph/fs/containers/provisioner/www/2009-04-04/user-data
-fi
+sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/pxe/user-data.php -O /mnt/ceph/fs/containers/provisioner/www/user-data/index.php
+
 sudo ln -s /mnt/ceph/fs/containers/provisioner/www/2009-04-04 /mnt/ceph/fs/containers/provisioner/www/latest
 echo "test" | sudo tee  /mnt/ceph/fs/containers/provisioner/www/latest/meta-data/instance-id/index.php
 echo "test" | sudo tee  /mnt/ceph/fs/containers/provisioner/www/latest/meta-data/hostname/index.php

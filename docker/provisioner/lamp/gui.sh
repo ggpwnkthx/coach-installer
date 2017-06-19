5bb003148a841396c2a8c2b04580bde537e3e2c8
ggpwnkthx <ibjessup@gmail.com>
2017-06-19 09:40:04 -0400

    Update gui.sh

--- docker/provisioner/lamp/gui.sh
+++ docker/provisioner/lamp/gui.sh
@@ -12,8 +12,9 @@ sudo chmod 777 /mnt/ceph/fs/containers/provisioner/www/application/config
 sudo chmod 777 /mnt/ceph/fs/containers/provisioner/www/application/files
 sudo sed -i 's/Install concrete5/Install COACH/g' /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
 sudo sed -i "s/<?php echo t('Site') ?>/<?php echo t('Cluster') ?>/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
-sudo sed -i  "s/pkgHandle == 'elemental_full'/pkgHandle == 'elemental_blank'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
-sudo sed -i  "s/'DB_SERVER', \['required' => 'required'\]/'DB_SERVER', '127.0.0.1',  \['required' => 'required'\]/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
-sudo sed -i  "s/'DB_USERNAME'/'DB_USERNAME', 'root'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
-sudo sed -i  "s/'DB_DATABASE'/'DB_DATABASE', 'db'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
-sudo sed -i  "s/'Edit Your Site'/'Start'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
+sudo sed -i "s/pkgHandle == 'elemental_full'/pkgHandle == 'elemental_blank'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
+sudo sed -i "s/'DB_SERVER', \['required' => 'required'\]/'DB_SERVER', '127.0.0.1',  \['required' => 'required'\]/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
+sudo sed -i "s/'DB_USERNAME'/'DB_USERNAME', 'root'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
+sudo sed -i "s/'DB_DATABASE'/'DB_DATABASE', 'db'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
+sudo sed -i "s/'Edit Your Site'/'Start'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/views/frontend/install.php
+sudo sed -i "s/'concrete.misc.login_redirect', 'DESKTOP'/'concrete.misc.login_redirect', 'HOMEPAGE'/g" /mnt/ceph/fs/containers/provisioner/www/concrete/src/Package/StartingPointPackage.php

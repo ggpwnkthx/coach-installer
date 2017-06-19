4c2a17808f857355938aecd37b5c3d19e7a9345e
ggpwnkthx <ibjessup@gmail.com>
2017-06-19 09:51:21 -0400

    Update deploy.sh

--- docker/provisioner/lamp/deploy.sh
+++ docker/provisioner/lamp/deploy.sh
@@ -23,4 +23,4 @@ sudo chmod +x /etc/ceph/provisioner-lamp.sh
 sudo wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/docker/provisioner/lamp/provisioner-lamp.service -O /etc/systemd/system/provisioner-lamp.service
 sudo systemctl daemon-reload
 sudo systemctl enable provisioner-lamp.service
-sudo systemctl start provisioner-lamp.service
+sudo systemctl restart provisioner-lamp.service

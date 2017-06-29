22fcc1a834272885410a1b0de3ac22c41c535c66
ggpwnkthx <ibjessup@gmail.com>
2017-06-29 14:42:29 -0400

    Update megacli.sh

--- hardware/storage/megacli.sh
+++ hardware/storage/megacli.sh
@@ -6,19 +6,19 @@ then
   then
     echo "MegaCLI alias found in /bin. No changes made."
   else
-    sudo ln -s /opt/MegaRAID/MegaCli/MegaCli64 /bin/MegaCli
+    ln -s /opt/MegaRAID/MegaCli/MegaCli64 /bin/MegaCli
     echo "MegaCLI alias was not found, so it was added."
   fi
 else
   # Install MegaCli and megaclisas-status
-  sudo apt-get -y install unzip alien dpkg-dev debhelper build-essential lshw python
+  apt-get -y install unzip alien dpkg-dev debhelper build-essential lshw python
   cd /tmp
   wget https://docs.broadcom.com/docs-and-downloads/raid-controllers/raid-controllers-common-files/8-07-14_MegaCLI.zip
   unzip 8-07-14_MegaCLI.zip
   cd Linux
-  sudo alien MegaCli*.rpm
-  sudo dpkg -i megacli*.deb
-  sudo ln -s /opt/MegaRAID/MegaCli/MegaCli64 /bin/MegaCli
+  alien MegaCli*.rpm
+  dpkg -i megacli*.deb
+  ln -s /opt/MegaRAID/MegaCli/MegaCli64 /bin/MegaCli
 fi
 
 if [ -f /opt/MegaRAID/MegaCli/megaclisas-status ]
@@ -28,12 +28,12 @@ then
   then
     echo "megaclisas-status alias found in /bin. No changes made."
   else
-    sudo ln -s /opt/MegaRAID/MegaCli/megaclisas-status /bin/megaclisas-status
+    ln -s /opt/MegaRAID/MegaCli/megaclisas-status /bin/megaclisas-status
     echo "megaclisas-status alias was not found, so it was added."
   fi
 else
   cd /opt/MegaRAID/MegaCli
-  sudo wget http://step.polymtl.ca/~coyote/dist/megaclisas-status/megaclisas-status
-  sudo chmod +x megaclisas-status
-  sudo ln -s /opt/MegaRAID/MegaCli/megaclisas-status /bin/megaclisas-status
+  wget http://step.polymtl.ca/~coyote/dist/megaclisas-status/megaclisas-status
+  chmod +x megaclisas-status
+  ln -s /opt/MegaRAID/MegaCli/megaclisas-status /bin/megaclisas-status
 fi

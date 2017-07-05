1a4e8bbec2cb75931d491048133d7e4a6c847478
ggpwnkthx <ibjessup@gmail.com>
2017-07-05 14:28:58 -0400

    Update deploy.sh

--- services/ajenti/deploy.sh
+++ services/ajenti/deploy.sh
@@ -7,9 +7,14 @@ fi
 
 curl https://raw.githubusercontent.com/ajenti/ajenti/master/scripts/install.sh | bash -s -
 
+systemctl stop ajenti.service
+
 if [ -z "$(cat /etc/ajenti/config.yml | grep 'restricted_user: $(whoami)')" ]
 then
   echo "restricted_user: $(whoami)" | sudo tee --append /etc/ajenti/config.yml
 fi
 
-systemctl restart ajenti.service
+pip uninstall ajenti.plugin.notepad
+pip install ajenti.plugin.network ajenti.plugin.datetime ajenti.plugin.power ajenti.plugin.traffic
+
+systemctl start ajenti.service

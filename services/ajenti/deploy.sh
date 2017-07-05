d09fcb8f1540a42fd196c72e8f57e75d71105419
ggpwnkthx <ibjessup@gmail.com>
2017-07-05 12:27:28 -0400

    Update deploy.sh

--- services/ajenti/deploy.sh
+++ services/ajenti/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-apt-get -y install build-essential python-pip python-dev python-lxml libffi-dev libssl-dev libjpeg-dev libpng-dev uuid-dev python-dbus
-pip install 'setuptools>=0.6rc11' 'pip>=6' wheel
-pip install ajenti-panel ajenti.plugin.dashboard ajenti.plugin.settings ajenti.plugin.plugins ajenti.plugin.terminal
+if [ -z "$(command -v curl)" ]
+then
+  apt-get -y install curl
+fi
+
+curl https://raw.githubusercontent.com/ajenti/ajenti/master/scripts/install.sh | bash -s -
 
 if [ -z "$(cat /etc/ajenti/config.yml | grep 'restricted_user: $(whoami)')" ]
 then
   echo "restricted_user: $(whoami)" | sudo tee --append /etc/ajenti/config.yml
 fi
 
-wget https://raw.githubusercontent.com/ggpwnkthx/coach/master/services/ajenti/ajenti.service -O /etc/systemd/system/ajenti.service
-systemctl enable ajenti.service
-systemctl start ajenti.service
+systemctl restart ajenti.service

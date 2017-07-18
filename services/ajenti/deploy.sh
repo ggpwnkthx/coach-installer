918f1181dbbfa150d3c2b856980ad274a38519bf
ggpwnkthx <ibjessup@gmail.com>
2017-07-18 08:18:29 -0400

    Update deploy.sh

--- services/ajenti/deploy.sh
+++ services/ajenti/deploy.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$(command -v curl)" ]
 then
-  apt-get -y install curl
+  apt-get -y install curl git
 fi
 
 curl https://raw.githubusercontent.com/ajenti/ajenti/master/scripts/install.sh | bash -s -
@@ -17,5 +17,9 @@ fi
 sed -i "s/{{customization.plugins.core.title || 'Ajenti'}}/{{customization.plugins.core.title || 'COACH'}}"/g /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/content/pages/index.html
 
 cp -r coach /usr/local/lib/python2.7/dist-packages/ajenti_plugin_coach
+rm /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor/fontawesome
+cd /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor
+git clone https://github.com/FortAwesome/Font-Awesome.git
+mv /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor/Font-Awesome /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor/fontawesome
 
 systemctl start ajenti.service

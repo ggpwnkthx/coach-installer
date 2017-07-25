ea725f42052a4ae18b1ed60ba3c2ac9cd6dea818
ggpwnkthx <ibjessup@gmail.com>
2017-07-25 10:38:46 -0400

    Update deploy.sh

--- services/ajenti/deploy.sh
+++ services/ajenti/deploy.sh
@@ -17,7 +17,7 @@ fi
 sed -i "s/{{customization.plugins.core.title || 'Ajenti'}}/{{customization.plugins.core.title || 'COACH'}}"/g /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/content/pages/index.html
 
 cp -r services/ajenti/coach /usr/local/lib/python2.7/dist-packages/ajenti_plugin_coach
-rm /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor/fontawesome
+rm -r /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor/fontawesome
 cwd=$(pwd)
 cd /usr/local/lib/python2.7/dist-packages/ajenti_plugin_core/resources/vendor
 git clone https://github.com/FortAwesome/Font-Awesome.git

76cf8f7e0b8353b7e940480505a85b2198957b6d
ggpwnkthx <ibjessup@gmail.com>
2017-08-01 17:13:10 -0400

    Create dev_mode.sh

---
+++ ajenti/dev_mode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+apt-get install -y nodejs
+npm -g install bower babel-cli babel-preset-es2015 babel-plugin-external-helpers less coffee-script angular-gettext-cli angular-gettext-tools
+apt-get install gettext
+pip install ajenti-dev-multitool
